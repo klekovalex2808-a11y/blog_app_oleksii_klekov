941e0588747ed128579e90d5d4244b80e7ac89dd
Oleksii Klekov <whizen2808@gmail.com>
2026-01-09 18:42:53 +0100

    Fix01: minor changes in Blog App

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,8 @@
 # Edited via GitHub!
 from django.db import models
 
+# Зміна в гілці fix01 для практикуму №6
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
